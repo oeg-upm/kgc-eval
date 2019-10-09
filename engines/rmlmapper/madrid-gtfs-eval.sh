1ce98484bed3cac731318ba815f83171326119f4
dachafra <dachafra@gmail.com>
2019-10-09 17:30:06 +0200

    dockercompose

--- engines/rmlmapper/madrid-gtfs-eval.sh
+++ engines/rmlmapper/madrid-gtfs-eval.sh
@@ -2,7 +2,7 @@
 
 rm /results/*
 echo "dataset,time">>/results/results-times-gtfs.csv
-echo "dataset,run,time">>/results/results-times-gtfs-detail.csv
+echo "dataset,run,results,time">>/results/results-times-gtfs-detail.csv
 
 for i in 1 5 10 50 100 500
 do
@@ -11,17 +11,19 @@ do
 	for j in 1 2 3 4 5
 	do
 		start=$(date +%s.%N)
-		timeout 10h java -jar /rmlmapper/rmlmapper.jar -m /mappings/gtfs-csv.rml.ttl -o /results/gtfs-$i-$j.ttl -d
+		timeout 10h java -jar /rmlmapper/rmlmapper.jar -m /mappings/gtfs-csv.rml.ttl -o /results/gtfs-$i-$j.nt -d
 		exit_status=$?
 		finish=$(date +%s.%N)
 		dur=$(echo "$finish - $start" | bc)
+		sort /results/gtfs-$i-$j.nt
 		if [ $exit_status -eq 124 ]
 		then
 			echo "gtfs-$i,TimeOut">>/results/results-times-gtfs.csv
 			total=0
 			break
 		else
-			echo "gtfs-$i,$run,$dur">>/results/results-times-gtfs-detail.csv
+			lines=$(< "/results/gtfs-$i-$j.nt" wc - l)
+			echo "gtfs-$i,$run,$lines,$dur">>/results/results-times-gtfs-detail.csv
 			total=$(($total + $dur))
 		fi
 	done
@@ -31,3 +33,4 @@ do
 		echo "gtfs-$i,$total">>/results/results-times-gtfs.csv
 	fi
 done	
+rm /data/*.csv

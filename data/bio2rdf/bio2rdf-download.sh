2beda46248fef841d40fd6425a7f93fe26cb1924
dachafra <dachafra@gmail.com>
2019-10-15 17:43:08 +0200

    download bio2rdf

--- data/bio2rdf/bio2rdf-download.sh
+++ data/bio2rdf/bio2rdf-download.sh
@@ -4,7 +4,7 @@ while IFS=, read -r dataset url
 do
 	if [[ $url =~ ^http.* ]]; then
 	    wget -O $dataset.zip $url
-	    unzip $dataset.zip -d $dataset
+	    unzip $dataset.zip
 	    rm $dataset.zip  
 	fi
 done < bio2rdf-url.csv

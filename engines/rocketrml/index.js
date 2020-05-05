7ddb918460670e774e704cec1984002eeda954e3
David Chaves <dachafra@gmail.com>
2020-05-05 17:26:22 +0200

    rocketrml index

--- engines/rocketrml/index.js
+++ engines/rocketrml/index.js
@@ -3,31 +3,31 @@ var fs = require('fs')
 
 const doMapping = async () => {
   try{
-    stops = fs.readFileSync('STOPS.csv', 'utf8')
-    stop_times = fs.readFileSync('STOP_TIMES.csv', 'utf8')
-    frequencies = fs.readFileSync('FREQUENCIES.csv', 'utf8')
-    calendar_dates = fs.readFileSync('CALENDAR_DATES.csv', 'utf8')
-    calendar = fs.readFileSync('CALENDAR.csv', 'utf8')
-    feed_info = fs.readFileSync('FEED_INFO.csv', 'utf8')
-    routes = fs.readFileSync('ROUTES.csv', 'utf8')
-    shapes = fs.readFileSync('SHAPES.csv', 'utf8')
-    trips = fs.readFileSync('TRIPS.csv', 'utf8')
-    agency = fs.readFileSync('AGENCY.csv', 'utf8')
-    mapping = fs.readFileSync('mapping.rml.ttl', 'utf8')
+    stops = fs.readFileSync('/data/STOPS.csv', 'utf8')
+    stop_times = fs.readFileSync('/data/STOP_TIMES.csv', 'utf8')
+    frequencies = fs.readFileSync('/data/FREQUENCIES.csv', 'utf8')
+    calendar_dates = fs.readFileSync('/data/CALENDAR_DATES.csv', 'utf8')
+    calendar = fs.readFileSync('/data/CALENDAR.csv', 'utf8')
+    feed_info = fs.readFileSync('/data/FEED_INFO.csv', 'utf8')
+    routes = fs.readFileSync('/data/ROUTES.csv', 'utf8')
+    shapes = fs.readFileSync('/data/SHAPES.csv', 'utf8')
+    trips = fs.readFileSync('/data/TRIPS.csv', 'utf8')
+    agency = fs.readFileSync('/data/AGENCY.csv', 'utf8')
+    mapping = fs.readFileSync('/mappings/gtfs-csv.rml.ttl', 'utf8')
   }catch(e){
     console.log('Error:', e.stack);
   }
   let inputFiles = {
-    "STOPS.csv": stops,
-    "STOP_TIMES.csv": stop_times,
-    "FREQUENCIES.csv": frequencies,
-    "CALENDAR_DATES.csv": calendar_dates,
-    "CALENDAR.csv": calendar,
-    "FEED_INFO.csv": feed_info,
-    "SHAPES.csv": shapes,
-    "TRIPS.csv": trips,
-    "AGENCY.csv": agency,
-    "ROUTES.csv": routes
+    "/data/STOPS.csv": stops,
+    "/data/STOP_TIMES.csv": stop_times,
+    "/data/FREQUENCIES.csv": frequencies,
+    "/data/CALENDAR_DATES.csv": calendar_dates,
+    "/data/CALENDAR.csv": calendar,
+    "/data/FEED_INFO.csv": feed_info,
+    "/data/SHAPES.csv": shapes,
+    "/data/TRIPS.csv": trips,
+    "/data/AGENCY.csv": agency,
+    "/data/ROUTES.csv": routes
   };
   const options = {
     toRDF: true,

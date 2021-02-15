aa85c271de3de1538caeec3b7c570d5c92ae6649
w0xter <luispozogilo@gmail.com>
2021-02-15 10:20:37 +0100

    Adding Result coutn

---
+++ scripts/.tempScript.sh
@@ -0,0 +1,1 @@
+python3 -m rdfizer -c ./tmp/config.ini
--- scripts/evaluate.py
+++ scripts/evaluate.py
@@ -7,14 +7,18 @@ def read_config(path):
     return config
 
 def evaluate(config):
-    f = open(config['output_path']+ 'result_times.csv', 'w')
-    f.write("run,elapsed_time,kernel_mode,user_mode,memory_max,memory_average\n")
-    f.close()    
     f = open('.tempScript.sh', 'w')
     f.write(config['cmd'] + '\n')
     f.close()
+
+    f = open('./result_times.csv', 'a')
+    f.write("run,elapsed_time,kernel_mode,user_mode,memory_max,memory_average,number_results\n")
     for i in range(config['rounds']):
-        os.system('/usr/bin/time -f "'+ str(i) + ',%e,%S,%U,%M,%K" --append -o ' + config['output_path'] + 'result_times.csv bash .tempScript.sh')
+        os.system('/usr/bin/time -f "'+ str(i) + ',%e,%S,%U,%M,%K" -o ./.temp.result_times.csv bash .tempScript.sh')
+        data = open('./.temp.result_times.csv').read()
+        number_results = os.popen("wc -l " + config['output_path'] + " | awk -F ' ' '{print $1}'").read()
+        data = data.replace('\n', '')
+        f.write(data + ',' + number_results)
 
 def main():
     parser = argparse.ArgumentParser()

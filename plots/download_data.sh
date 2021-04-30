c2b64d02847d0d5fa645466334e009fdaa49ece4
anaigmo <ana.iglesias.molina@gmail.com>
2021-04-30 09:58:52 +0200

    rm script

--- plots/download_data.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-wget 'https://docs.google.com/spreadsheets/d/1q8-vlRfGoEiZWnySdce92UwqFMKYge1oc60P_4Izcrw/gviz/tq?tqx=out:csv&sheet=GTFS-1' -O 'data/Results - Time - GTFS-1.csv'
-wget 'https://docs.google.com/spreadsheets/d/1q8-vlRfGoEiZWnySdce92UwqFMKYge1oc60P_4Izcrw/gviz/tq?tqx=out:csv&sheet=GTFS-10' -O 'data/Results - Time - GTFS-10.csv'
-wget 'https://docs.google.com/spreadsheets/d/1q8-vlRfGoEiZWnySdce92UwqFMKYge1oc60P_4Izcrw/gviz/tq?tqx=out:csv&sheet=GTFS-100' -O 'data/Results - Time - GTFS-100.csv'
-wget 'https://docs.google.com/spreadsheets/d/1q8-vlRfGoEiZWnySdce92UwqFMKYge1oc60P_4Izcrw/gviz/tq?tqx=out:csv&sheet=GTFS-1000' -O 'data/Results - Time - GTFS-1000.csv'
